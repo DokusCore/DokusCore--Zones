298d308cd44ae48363f93072a6fb06422f179a45
DevDokus <49053928+DevDokus@users.noreply.github.com>
2023-01-20 16:55:53 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@ description 'DokusCore | Manazing your own custom zones'
 author 'http://DokusCore.com'
 fx_version "adamant"
 games {"rdr3"}
--- version '1.0.1'
-version '-----' -- For Development
+version '1.1.0'
+-- version '-----' -- For Development
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 client_scripts {
